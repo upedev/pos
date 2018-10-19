61f0148b35c768d809d2d92d0d0340aefbf4a08f
vipulsinghbrahmania <vipulsinghbrahmania@gmail.com>
2018-10-19 22:51:08 +0530

    added mysql, gitignore

--- pos/settings.py
+++ pos/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posapp.apps.PosappConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,13 +76,15 @@ WSGI_APPLICATION = 'pos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'posdb',
+        'USER': 'admin',
+        'PASSWORD': '3er4#ER$',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
-
-# Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
